cefc73529438c5e69479f23a220c2d252fadf3f8
Divya Kumari <divyakumari0093@gmail.com>
2026-05-11 15:31:33 +0530

    Dockerfile added

--- src/js/TableClass.js
+++ src/js/TableClass.js
@@ -1,8 +1,8 @@
-import {runAlgo,generateMaze} from './GraphAlgos.js';
+import { runAlgo, generateMaze } from './GraphAlgos.js';
 
-export default class Table{
+export default class Table {
 
-    constructor(id,active=false) {
+    constructor(id, active = false) {
         this.id = id;
         this.graphAlgo = null;
         this.mazeAlgo = null;
@@ -13,85 +13,85 @@ export default class Table{
     }
 
 
-    activate(){
+    activate() {
         this.active = true;
         document.getElementById(this.id).style.display = 'flex';
         this.updateTitle();
     }
 
 
-    deactivate(){
+    deactivate() {
         this.active = false;
         this.inProgress = false;
         this.mazeAlgo = null;
         this.graphAlgo = null;
-        document.getElementById(this.id).style.display = 'none'; 
+        document.getElementById(this.id).style.display = 'none';
         this.updateTitle();
-        if(this.grid !== null){
-           this.cleanBoard();
+        if (this.grid !== null) {
+            this.cleanBoard();
         }
     }
 
-    updateTitle(){
-        if(this.graphAlgo){
+    updateTitle() {
+        if (this.graphAlgo) {
             document.querySelector(`#${this.id} .algo-title .graph-algo`).innerText = this.graphAlgo
-        }else{
+        } else {
             document.querySelector(`#${this.id} .algo-title .graph-algo`).innerText = '';
         }
-        if(this.mazeAlgo){
+        if (this.mazeAlgo) {
             document.querySelector(`#${this.id} .algo-title .maze-algo`).innerText = this.mazeAlgo;
-        }else{
+        } else {
             document.querySelector(`#${this.id} .algo-title .maze-algo`).innerText = '';
         }
     }
 
 
-    runAlgo(diagonal,mazeSpeed,graphSpeed,manhattan){
-        if(this.inProgress) return;
+    runAlgo(diagonal, mazeSpeed, graphSpeed, manhattan) {
+        if (this.inProgress) return;
 
         this.inProgress = true;
 
         document.getElementById('panel-toggle').disabled = true;
 
-        let [start,end] = this.getGrid();
-        
+        let [start, end] = this.getGrid();
+
         let mazeDelay = 0;
-        if(this.mazeAlgo){
+        if (this.mazeAlgo) {
             this.cleanBoard();
-            [start,end] = this.getGrid();
-           mazeDelay = generateMaze(this.mazeAlgo, this.grid,mazeSpeed)
+            [start, end] = this.getGrid();
+            mazeDelay = generateMaze(this.mazeAlgo, this.grid, mazeSpeed)
         }
 
-        setTimeout(()=>{
-            if(this.graphAlgo){
-                let [time,blocks, delay] = runAlgo(this.graphAlgo, this.grid, start, end, diagonal,graphSpeed,manhattan);
-                
-                if(time!==-1) {
-                    setTimeout(()=>{
-                        this.showTimeBlock(time,blocks);
+        setTimeout(() => {
+            if (this.graphAlgo) {
+                let [time, blocks, delay] = runAlgo(this.graphAlgo, this.grid, start, end, diagonal, graphSpeed, manhattan);
+
+                if (time !== -1) {
+                    setTimeout(() => {
+                        this.showTimeBlock(time, blocks);
                         this.inProgress = false;
                         document.getElementById('panel-toggle').disabled = false;
                     }, delay);
-                    
-                }else{
+
+                } else {
                     this.showMessage('Path Not Found!');
                     this.inProgress = false;
                     document.getElementById('panel-toggle').disabled = false;
                     return mazeDelay;
                 }
-            
-            }else{
+
+            } else {
                 this.inProgress = false;
                 document.getElementById('panel-toggle').disabled = false;
             }
-        },mazeDelay);
-        
+        }, mazeDelay);
+
         //this.inProgress = false;
         //document.getElementById('panel-toggle').disabled = false;
     }
 
 
-    getGrid(){
+    getGrid() {
         const grid = []
         let start;
         let end;
@@ -99,88 +99,89 @@ export default class Table{
 
         let firstHiddenRowsCount = 0;
         let hiddenRowsExist = false;
-        
-        for(let i=0; i<gridEle.children.length; i++){
-            
-            if(window.getComputedStyle(gridEle.children[i]).display === "none"){
-                hiddenRowsExist =true;
+
+        for (let i = 0; i < gridEle.children.length; i++) {
+
+            if (window.getComputedStyle(gridEle.children[i]).display === "none") {
+                hiddenRowsExist = true;
                 continue;
-            } 
-            if ( hiddenRowsExist && firstHiddenRowsCount===0) firstHiddenRowsCount = i;
+            }
+            if (hiddenRowsExist && firstHiddenRowsCount === 0) firstHiddenRowsCount = i;
             const row = gridEle.children[i].children;
             const currentRow = []
 
             let firstHiddenColsCount = 0;
             let hiddenColsExist = false;
 
-            for(let j=0; j<row.length; j++){
-                if(window.getComputedStyle(row[j]).display === "none"){
+            for (let j = 0; j < row.length; j++) {
+                if (window.getComputedStyle(row[j]).display === "none") {
                     hiddenColsExist = true;
                     continue;
-                } 
+                }
 
-                if (hiddenColsExist && firstHiddenColsCount===0) firstHiddenColsCount = j;
+                if (hiddenColsExist && firstHiddenColsCount === 0) firstHiddenColsCount = j;
 
                 currentRow.push(row[j]);
-                if (row[j].classList.contains('start')){
-                    start=[i-firstHiddenRowsCount,j-firstHiddenColsCount];
-                } 
+                if (row[j].classList.contains('start')) {
+                    start = [i - firstHiddenRowsCount, j - firstHiddenColsCount];
+                }
                 if (row[j].classList.contains('end')) {
-                    end=[i-firstHiddenRowsCount,j-firstHiddenColsCount];
-                }   
+                    end = [i - firstHiddenRowsCount, j - firstHiddenColsCount];
+                }
             }
             grid.push(currentRow);
         }
         this.grid = grid;
-        
-        return [ start, end]
+
+        return [start, end]
     }
 
 
-    cleanGrid(){
-        if (this.inProgress)  return
+    cleanGrid() {
+        if (this.inProgress) return
         document.querySelector(`#${this.id} .duration-msg`).style.display = 'none';
-        this.grid.map(row=>row.map(cell=>{
-            if (cell.classList.contains('visited')){
+        this.grid.map(row => row.map(cell => {
+            if (cell.classList.contains('visited')) {
                 cell.classList.remove('visited');
             }
-            if (cell.classList.contains('shortest-path')){
+            if (cell.classList.contains('shortest-path')) {
                 cell.classList.remove('shortest-path');
             }
         }))
     }
 
 
-    cleanBoard(){
-        if (this.inprogress) return
+    cleanBoard() {
+        if (this.inProgress) return
         this.cleanGrid();
-        this.grid.map(row=>row.map(cell=>{
-            if (cell.classList.contains('wall')){
+        this.grid.map(row => row.map(cell => {
+            if (cell.classList.contains('wall')) {
                 cell.classList.remove('wall');
             }
-            if (cell.classList.contains('weight')){
+            if (cell.classList.contains('weight')) {
                 cell.classList.remove('weight');
             }
         }))
     }
 
-    resetBoard(){
-        if(this.inprogress) return;
+    resetBoard() {
+        if (this.inProgress) return;
         this.cleanBoard();
-        this.grid.map(row=>row.map(cell=>{
-        if(cell.classList.contains('start')){
-            cell.classList.remove('start')
-        }
-        if(cell.classList.contains('end')){
-            cell.classList.remove('end')
-        }
-        document.querySelector(`#${this.id} #start`).className='start';
-        document.querySelector(`#${this.id} #end`).className='end';
-    }))}
+        this.grid.map(row => row.map(cell => {
+            if (cell.classList.contains('start')) {
+                cell.classList.remove('start')
+            }
+            if (cell.classList.contains('end')) {
+                cell.classList.remove('end')
+            }
+            document.querySelector(`#${this.id} #start`).className = 'start';
+            document.querySelector(`#${this.id} #end`).className = 'end';
+        }))
+    }
 
 
 
-    showTimeBlock(time,blocks){
+    showTimeBlock(time, blocks) {
         document.querySelector(`#${this.id} .duration-msg`).innerHTML = `<div class="duration">Duration : <span>${time}</span> ms</div>
         <div class="blocks">Blocks: <span>${blocks} </span></div>
         `;
@@ -189,7 +190,7 @@ export default class Table{
 
     }
 
-    showMessage(msg){
+    showMessage(msg) {
         document.querySelector(`#${this.id} .duration-msg`).style.display = 'block';
         document.querySelector(`#${this.id} .duration-msg`).style.background = 'red';
         document.querySelector(`#${this.id} .duration-msg`).innerHTML = msg;
